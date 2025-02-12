77b1c1e566a042681b6717f13ff4ab3686935221
MarcusCheecham <6001808@spsd.sk.ca>
2025-02-12 12:19:46 -0600

    Interactive Scene project - Added background

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -3,32 +3,62 @@
 // Febuary 11th, 2025
 //
 // And interactive scene that allows your to use both mouse and keyboard inputs to change and interact with the screen
-let posX, posY;
+let count = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(24);
-  posX = random(0, width);
-  posY = 0;
+  frameRate(60);
+
 }
 
 function draw() {
-  background(220);
+  background(89, 211, 255);
+  //sun
+  fill(255, 255, 0);
+  arc(width*0.93, height*0.15, 120, 120, 0, PI*2, PIE, 2);
+  circle(width*0.93, height*0.15, 100);
+
+
+  // Sky / Clouds
+  noStroke();
+  fill(255);
+  rotate(50.5);
+  ellipse(width*0.11, height*0.04, width*0.10, height*0.10);
+  rotate(-101);
+  ellipse(width*0.08, height*0.15, width*0.10, height*0.10);
+  rotate(50.5);
+  ellipse(width*0.10, height*0.10, width*0.11, height*0.10);
+
+
+
+  // Ground
+  stroke(1);
+  fill(0, 200, 0);
+  ellipse(width*0.65, height*0.85, width*0.90, height*0.70);
+  fill(0, 230, 0);
+  ellipse(width*0.25, height, width*0.90, height*0.70);
+  fill(0, 255, 0);
+  ellipse(width*0.75, height*1.15, width*0.90, height*0.70);
+
+
+  
+
+  // particalSystem();
+  fill(0, 0, 0);
   textSize(20);
   text(frameCount, mouseX, mouseY);
-  for (let i = 0; i <= 2; i++) {
-    particalSystem();
+  if (frameCount >= 60) {
+    frameCount = 0;
   }
 }
 
-function particalSystem() {
-  if (posY >= height || posX >= width) {
-    posX = random(0, width);
-    posY = 0;
-  }
-  noStroke();
-  fill(255, 255, 255);
-  circle(posX, posY, 10);
-  posY+= 2;
-  posX+= 1;
-}
+// function particalSystem() {
+//   let snow = [];
+//   let snowPos = [];
+//   for (count <= 1; count++) {
+//     fill(0, 0, 255)
+//     snow.push(circle(random(0, width), 0, 10));
+//   }
+//   fill(255, 0, 0);
+//   rect(0, 0, width, 10);
+// }
