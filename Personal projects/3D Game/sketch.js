24e28c06dd088ce482c9282af5ce9b30fd51a120
MarcusCheecham <6001808@spsd.sk.ca>
2025-03-23 14:17:20 -0600

    Working on player rotation and movement

--- Personal projects/3D Game/sketch.js
+++ Personal projects/3D Game/sketch.js
@@ -6,23 +6,48 @@
 // - describe what you did to take this project "above and beyond"
 
 let playerX = 0;  let playerY = -100; let playerZ = 0
+let camList = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
+  cam2 = createCamera();
   player = createCamera();
   player.setPosition(playerX, playerY, playerZ)
+  cam2.setPosition(playerX - 100, playerY, playerX) 
+  
 }
 
 function draw() {
   debugMode();
   background(220);
   cameraControls();
+  sphere(10);
+  
+}
 
-
+function keyPressed() {
+  if (keyCode === "W") {
+    playerX;
+  }
 }
 
 function cameraControls() {
   if (keyIsDown(LEFT_ARROW) === true) {
-    player.pan(-1)
+    player.pan(2)
   }
+  if (keyIsDown(RIGHT_ARROW) === true) {
+    player.pan(-2)
+  }
+  if (keyIsDown(UP_ARROW) === true) {
+    player.tilt(-2)
+  }
+  if (keyIsDown(DOWN_ARROW) === true) {
+    player.tilt(2)
+  }
+
+  if (keyIsDown("W") === true) {
+    playerX+= 10;
+  }
+
 }
