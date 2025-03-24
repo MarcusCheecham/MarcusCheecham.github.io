2c5620031ce889b094aa27eb1009cec375afc925
MarcusCheecham <6001808@spsd.sk.ca>
2025-03-24 13:01:56 -0600

    Working on 3D movement local to the world

--- Personal projects/3D Game/sketch.js
+++ Personal projects/3D Game/sketch.js
@@ -5,49 +5,75 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let player;
 let playerX = 0;  let playerY = -100; let playerZ = 0
 let camList = [];
+let turningDegree = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
-  cam2 = createCamera();
   player = createCamera();
-  player.setPosition(playerX, playerY, playerZ)
-  cam2.setPosition(playerX - 100, playerY, playerX) 
+  player.setPosition(playerX, playerY, playerZ);
+  perspective(2 * atan(height / 2 / 800), width / height, 0.01, 5000);
   
 }
 
 function draw() {
   debugMode();
+
   background(220);
+
   cameraControls();
-  sphere(10);
+
+  translate(0, -100, -100);
+  sphere(10, 10, 10);
   
 }
 
-function keyPressed() {
-  if (keyCode === "W") {
-    playerX;
-  }
-}
-
 function cameraControls() {
-  if (keyIsDown(LEFT_ARROW) === true) {
-    player.pan(2)
-  }
-  if (keyIsDown(RIGHT_ARROW) === true) {
-    player.pan(-2)
-  }
-  if (keyIsDown(UP_ARROW) === true) {
-    player.tilt(-2)
-  }
-  if (keyIsDown(DOWN_ARROW) === true) {
-    player.tilt(2)
-  }
+  if (keyIsPressed) {
 
-  if (keyIsDown("W") === true) {
-    playerX+= 10;
-  }
+    if (keyIsDown(LEFT_ARROW) === true) {
+      player.pan(2);
+      turningDegree -= 2;
+    }
+    if (keyIsDown(RIGHT_ARROW) === true) {
+      player.pan(-2);
+      turningDegree += 2;
+    }
 
+    if (turningDegree <= -1) {
+      turningDegree = 360 - 2;
+    } else if (turningDegree >= 361) {
+      turningDegree = 0 + 2;
+    }
+
+    if (keyIsDown(UP_ARROW) === true) {
+      player.tilt(-2);
+    }
+    if (keyIsDown(DOWN_ARROW) === true) {
+      player.tilt(2);
+    }
+
+    if (keyIsDown(87) === true) {
+      playerZ-= 10;
+
+      playerZ += (sin(turningDegree) * cos(turningDegree)) * 2;
+      playerX += (sin(turningDegree) * cos(turningDegree)) * 2;
+
+    }
+    if (keyIsDown(83) === true) {
+      playerZ+= 10;
+    }
+    if (keyIsDown(65) === true) {
+      playerX -= 10;
+    }
+    if (keyIsDown(68) === true) {
+      playerX += 10;
+    }
+
+    player.setPosition(playerX, playerY, playerZ);
+
+  }
 }
