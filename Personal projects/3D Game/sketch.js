4a5e283f975792421bbbe7263e85ba4cc15cb1c7
MarcusCheecham <6001808@spsd.sk.ca>
2025-03-27 20:51:36 -0600

    Movement fixed and fixed weird outlining being bulky

--- Personal projects/3D Game/sketch.js
+++ Personal projects/3D Game/sketch.js
@@ -6,16 +6,18 @@
 // - describe what you did to take this project "above and beyond"
 
 let player;
-let playerX = 0;  let playerY = -100; let playerZ = 0
+let playerX = 0;  let playerY = -100; let playerZ = 0;
 let camList = [];
 let turningDegree = 0;
+let speed = 5;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
   player = createCamera();
   player.setPosition(playerX, playerY, playerZ);
-  perspective(2 * atan(height / 2 / 800), width / height, 0.01, 5000);
+  perspective(2 * atan(height / 2 / 500), width / height, 0.01 * 800, 10 * 800);
+  linePerspective(false);
   
 }
 
@@ -27,6 +29,7 @@ function draw() {
   cameraControls();
 
   translate(0, -100, -100);
+  fill(0, 255, 0);
   sphere(10, 10, 10);
   
 }
@@ -56,30 +59,65 @@ function cameraControls() {
       player.tilt(2);
     }
 
-    if (keyIsDown(87) === true) {
+    if ((keyIsDown(87) && (keyIsDown(65) || keyIsDown(68))) || (keyIsDown(83) && (keyIsDown(68) || keyIsDown(65)))) {
 
-      if (turningDegree >= 0 && turningDegree <= 180) {
-        playerX -= (sin(turningDegree) * cos(turningDegree + 90)) * 10;
-      } else {
-        playerX += (sin(turningDegree) * cos(turningDegree + 90)) * 10;
-      }
+      if (keyIsDown(87) === true) { // W
+
+        playerX += sin(turningDegree) * speed/1.5;
   
-      if (turningDegree <= 90 && turningDegree >= 270) {
-        playerZ -= (sin(turningDegree) * cos(turningDegree + 90)) * 10;
-      } else {
-        playerZ += (sin(turningDegree) * cos(turningDegree + 90)) * 10;
+        playerZ -= cos(turningDegree) * speed/1.5;
       }
+      if (keyIsDown(83) === true) { // S
   
+        playerX -= sin(turningDegree) * speed/1.5;
+  
+        playerZ += cos(turningDegree) * speed/1.5;
+      }
+      if (keyIsDown(65) === true) { // A
+
+        playerX -= cos(turningDegree) * speed/1.5;
+  
+        playerZ -= sin(turningDegree) * speed/1.5;
+        
+      }
+      if (keyIsDown(68) === true) { // D
+  
+        playerX += cos(turningDegree) * speed/1.5;
+  
+        playerZ += sin(turningDegree) * speed/1.5;
+  
+      }
+
+    } else {
+
+    if (keyIsDown(87) === true) { // W
+
+      playerX += sin(turningDegree) * speed;
+
+      playerZ -= cos(turningDegree) * speed;
+    }
+    if (keyIsDown(83) === true) { // S
+
+      playerX -= sin(turningDegree) * speed;
+
+      playerZ += cos(turningDegree) * speed;
 
     }
-    if (keyIsDown(83) === true) {
-      playerZ+= 10;
+    if (keyIsDown(65) === true) { // A
+
+      playerX -= cos(turningDegree) * speed;
+
+      playerZ -= sin(turningDegree) * speed;
+      
     }
-    if (keyIsDown(65) === true) {
-      playerX -= 10;
+    if (keyIsDown(68) === true) { // D
+
+      playerX += cos(turningDegree) * speed;
+
+      playerZ += sin(turningDegree) * speed;
+
     }
-    if (keyIsDown(68) === true) {
-      playerX += 10;
+
     }
 
     player.setPosition(playerX, playerY, playerZ);
