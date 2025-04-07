a41b6e399e1a96c80ced809f33f04d48e1541684
MarcusCheecham <6001808@spsd.sk.ca>
2025-04-07 12:02:19 -0600

    Demo 18

---
+++ --→ Demos ←--/18 - Block Pusher/sketch.js
@@ -0,0 +1,81 @@
+// Block Pusher Starter
+// Your Name
+// Date
+
+
+let tiles = [];  //0 → grass  1 → chicken   2 → cow  3 → star
+let level = [
+  [0, 1, 0, 3, 0],
+  [1, 0, 0, 1, 0],
+  [0, 1, 1, 0, 0],
+  [0, 1, 0, 0, 0],
+  [0, 0, 1, 0, 0]
+]
+const COLUMNS = 5; const ROWS = 5; let TILE_SIZE = 100;
+let playerX = 3; let playerY = 4;
+
+function preload() {
+  for (let i = 0; i < 4; i++) {
+    tiles.push(loadImage("assets/" + i + ".png"));
+  }
+}
+
+function swap(x1, y1, x2, y2) {
+  // modify the gameboard; switch two items
+  let temp = level[y1][x1];
+  level[y1][x1] = level[y2][x2];
+  level[y2][x2] = temp;
+}
+
+function keyPressed() {
+  // try a single action per keypresses
+  if(keyCode === UP_ARROW && playerY > 0) {
+    if (level[playerY - 1][playerX] === 0) {
+      swap(playerX, playerY, playerX, playerY - 1);
+      playerY--;
+    } else if (level[playerY - 1][playerX] === 1) {
+      // Is there room for it to be pushed??
+      if (playerY - 2 >= 0 && level[playerY - 2][playerX] === 0) {
+        // Grass above the chicken - allowed to push
+        swap(playerX, playerY - 1, playerX, playerY - 2);
+        swap(playerX, playerY, playerX, playerY - 1);
+        playerY--;
+      }
+    }
+
+
+
+  } else if (keyCode === DOWN_ARROW && playerY < COLUMNS) {
+    swap(playerX, playerY, playerX, playerY + 1);
+    playerY++;
+  } else if (keyCode === RIGHT_ARROW && playerX < ROWS) {
+    swap(playerX, playerY, playerX + 1, playerY);
+    playerX++;
+  } else if (keyCode === LEFT_ARROW && playerX > 0) {
+    swap(playerX, playerY, playerX - 1, playerY);
+    playerX--; 
+  }
+}
+
+function renderBoard() {
+  // interpret data in our 2D array (level), place images
+  // on canvas.
+  for (let x = 0; x < COLUMNS; x++) {
+    for (let y = 0; y < ROWS; y++) {
+      let type = level[y][x];
+      let currentImage = tiles[type];
+      image(currentImage, x * TILE_SIZE, y * TILE_SIZE);
+    }
+  }
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  level[playerY][playerX] = 2;
+
+}
+
+function draw() {
+  background(220);
+  renderBoard();
+}
