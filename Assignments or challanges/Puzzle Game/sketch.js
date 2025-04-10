5ebb788f0ded27d703e8b14381301b27c469f159
MarcusCheecham <6001808@spsd.sk.ca>
2025-04-10 11:19:08 -0600

    Arrow Function Explination

--- Assignments or challanges/Puzzle Game/sketch.js
+++ Assignments or challanges/Puzzle Game/sketch.js
@@ -63,8 +63,97 @@ function mousePressed() {
   }
 }
 
-function winCondition(value) {
-  return (value % 255) === 0;
+function winCondition() {
+  // Uses Arrow functions inorder to quickly check if every suqare is black or white.
+
+  return grid.every(row => row.every(item => item === 255)) || grid.every(row => row.every(item => item === 0));
+
+  // Deeper explination below:
+  /*
+  
+    // The every() method checks if all array values pass a test.  -W3Schools.
+    // This becomes complicated when it comes to 2D arrays since it checks the arrays inside the array
+    // to see if they pass the check instead of the int themselves.
+
+    Example:
+
+        ---- 1D Arrays ----
+      let array = [18, 20, 13, 80];
+      function overTen(value) {
+        return value > 10;
+      }
+      array.every(overTen); // OUTPUTS TRUE
+
+        ---- 2D Arrays ----
+      let array = [[18, 20, 13, 80], [50, 23, 85, 53]];
+      function overTen(value) {
+        return value > 10;
+      }
+      array.every(overTen); // OUTPUTS FALSE
+
+    // In order to truly check each individual array inside the array.
+    // We could use a <for loop> but this is weird using a for loop when array.every() already loops throught the entire 1D array
+
+    This is how it would practically look like:
+
+      function winningCondition(value) {
+        return value === 255;
+      }
+
+    // So how could we use array.every() to the best to our ability?
+    // We use Arrow Functions! ->  "=>" This thing
+    // Arrow functions were introduced in 2015 and became fully supported in 2016.
+    // Arrow functions allow you to quickly make functions and simplify functions.
+
+    Example:
+
+      ---- Default Functions ----
+    function greeting(name) {
+      return "Hello", name;
+    }
+
+      ---- Arrow Functions ----
+    greeting = (name) => "Hello", name;
+    
+    // You could also do this and gain the same output but it does not have a variable name linked with it.
+
+    name => "Hello", name;
+
+    // A great example would be for this type of arrow function writing is...
+
+    const materials = ["Hydrogen", "Helium", "Lithium", "Beryllium"];
+    console.log(materials.map((material) => material.length));
+    // Expected output: Array [8, 6, 7, 9]
+
+    // This will print out the number of letters in the string from the array.
+    // This is because the word "material" on the left of the arrow becomes a child to "materials" (Keep note of the "s" at the end of the name)
+    // "material" is now identified as the item inside the array itself and not the total array.
+    // With this we can do even more stuff like:
+
+    grid.every(row => row.every(item => item === 255));
+
+    grid = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]];
+
+    row = [1, 2, 3, 4, 5]; OR [6, 7, 8, 9, 10]; OR [11, 12, 13, 14, 15];
+
+    item = 1 - 15;
+
+    // But you may ask, how is it changing to check each int?
+    // well array.every() already loops through arrays to see if it is equal to the test you put it through.
+    // The reason why we need to create "item" and cannot just use "row" like this:
+
+    grid.every(row => row === 255)); // ERROR
+    
+    // Is because we need a variable for the number to be stored to be used to compaire with the test.
+    // If not, we would be compairing an entire array to a single int, bool, or string. Which does not work.
+
+    This is how it would look like to the system:
+
+    [1, 2, 3, 4, 5] === 255; // OUTPUTS FALSE
+
+    // array.every() acts like a <for loop> without having to write several lines to make a <for loop>
+
+  */
 }
 
 function flip(x, y) {
@@ -78,9 +167,16 @@ function draw() {
   // interpet the information in the 2D array, and draw a grid of colors on the screen to reflect it.
   renderGrid();
   
-  for (let y of grid) {
-    for (let x of grid) {
-      console.log(y.x.every(winCondition));
-    }
-  }
+  // for (let y = grid.length; y > 0; y--) {
+    // for (let x = grid.; x.length > 0; x--) {
+      // console.log(y.every(winCondition));
+      // }
+  // }
+
+  console.log();
+
+// const materials = ["Hydrogen", "Helium", "Lithium", "Beryllium"];
+// console.log(materials.map((material) => material.length));
+// // Expected output: Array [8, 6, 7, 9]
+// from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 }
