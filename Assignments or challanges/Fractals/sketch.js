47b44515cfeae6f63f7b0d6258490fc1cca7a0b2
MarcusCheecham <6001808@spsd.sk.ca>
2025-04-17 11:32:43 -0600

    3D fractal lighting done

--- Assignments or challanges/Fractals/sketch.js
+++ Assignments or challanges/Fractals/sketch.js
@@ -1,15 +1,38 @@
 // Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Marcus Cheecham
+// April 17th, 2025
+
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(degrees);
 }
 
 function draw() {
   background(220);
+  orbitControl();
+  colorMode(RGB, 255);
+  specularColor(100, 100, 100);
+  specularMaterial(150, 150, 150);
+
+  noStroke();
+  colorMode(HSB, 100);
+  directionalLight(50, 100, 100, -1, 1, 0);
+  directionalLight(100, 0, 100, 1, -1, 0);
+
+  pentagonTorus(0, 0, 30);
+  sphere(5, 10, 10);
+}
+
+function pentagonTorus(x, y, size) {
+  if (size >= 25) {
+
+    translate(x, y);
+    torus(size, 15, 3, 3);
+    sphere(5, 10, 10);
+    rotateZ(90);
+    pentagonTorus(x + 50, y+50, size-5);
+
+  }
 }
