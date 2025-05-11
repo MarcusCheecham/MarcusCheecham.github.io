da047c87d37b3d1906e5bfb61f5370ba0757ba6b
MarcusCheecham <6001808@spsd.sk.ca>
2025-05-11 02:58:01 -0600

    Working on Ray casting

--- Assignments or challanges/Capstone/AYOOYM/Scripts/HeadMovements.gd
+++ Assignments or challanges/Capstone/AYOOYM/Scripts/HeadMovements.gd
@@ -1,23 +1,13 @@
+extends Node3D
 
+var sens = 0.2
 
-const CAM_SENS = 0.006
-var head = self
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
-func _input(event):
+func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * CAM_SENS)
-		self.Camera3D.rotate_x(-event.relative.y * CAM_SENS)
-		Camera3D.rotation.x = clampf(Camera3D.rotation.x, deg_to_rad(-90.0), deg_to_rad(90.0))
-		
-	elif event is InputEventKey and event.pressed and event.keycode == KEY_ESCAPE:
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		
+		rotation_degrees.y -= event.relative.x * sens
+		rotation_degrees.y = clamp(rotation_degrees.y, -80.0, 80.0)
+		$Camera3D.rotation_degrees.x -= event.relative.y * 0.2
+		$Camera3D.rotation_degrees.x = clamp(
+			$Camera3D.rotation_degrees.x, -60.0, 60.0
+		)
+
--- Assignments or challanges/Capstone/AYOOYM/Scripts/RayCast3D.gd
+++ Assignments or challanges/Capstone/AYOOYM/Scripts/RayCast3D.gd
@@ -2,6 +2,30 @@ extends RayCast3D
 
 
 func _physics_process(delta):
-	if is_colliding():
-		var hit = get_collider()
-		print(hit.name)
+	pass
+		
+func _input(event):
+	if event.is_action_pressed("CamSwitch"):
+		scale.y = 50
+		print("Extend")
+	elif event.is_action_released("CamSwitch"):
+		scale.y = 1
+		print("Shortend")
+		
+		
+#func _input(event):
+	#if event.is_action_pressed("CamSwitch") && is_colliding():
+		#print(get_collider().name)
+
+		
+		#_shoot()
+#
+#func _shoot():
+	#var space = get_world_3d().direct_space_state
+	#var query = PhysicsRayQueryParameters3D.create($Camera3D.global_position,
+		#$Camera3D.global_position - $Camera3D.global_transform.basis.z * 100)
+	#var collision = space.intersect_ray(query)
+	#if collision:
+		#$CanvasLayer/Label.text = collision.collider.name
+	#else:
+		#$CanvasLayer/Label.text = ""
