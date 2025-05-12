50546a0e0b3d4fb65182ad43083efb5464713021
MarcusCheecham <6001808@spsd.sk.ca>
2025-05-12 13:01:59 -0600

    Gotten RayCasting to work
    Now working on using it to switch cameras

--- Assignments or challanges/Capstone/AYOOYM/Scripts/RayCast3D.gd
+++ Assignments or challanges/Capstone/AYOOYM/Scripts/RayCast3D.gd
@@ -1,15 +1,27 @@
 extends RayCast3D
 
+const RAY_LENGTH = 1000
 
 func _physics_process(delta):
+	if is_colliding():
+		var space_state = get_world_3d().direct_space_state
+		var cam = get_node("..")
+		var mousepos = get_viewport().get_mouse_position()
 
+		var origin = cam.project_ray_origin(mousepos)
+		var end = origin + cam.project_ray_normal(mousepos) * RAY_LENGTH
+		var query = PhysicsRayQueryParameters3D.create(origin, end)
+		query.collide_with_areas = true
+
+		var result = space_state.intersect_ray(query)
+		print(result)
 		
 func _input(event):
 	if event.is_action_pressed("CamSwitch"):
-		target_position.z = 50
+		target_position.z = -50
 		print("Extend")
 	elif event.is_action_released("CamSwitch"):
-		target_position.z = 1
+		target_position.z = -0
 		print("Shortend")
 		
 		
