51059dc9a08f07c268b1ac157bcf0a8adcbe067d
MarcusCheecham <6001808@spsd.sk.ca>
2025-05-14 12:17:08 -0600

    Attempting on working on wall collision

--- Assignments or challanges/Capstone/AYOOYM/Scripts/CamSwitch.gd
+++ Assignments or challanges/Capstone/AYOOYM/Scripts/CamSwitch.gd
@@ -1,13 +1,15 @@
 extends Area3D
 
 func _switch():
-	print("Tagged")
-	var Countdown = get_node("../../UI/RichTextLabel")
-	for i in range(3):
-		Countdown.text = str(i + 1)
-		await get_tree().create_timer(1.0).timeout
-		print(i)
-
 	var cam = get_node("../CamNeck/Camera3D")
-	cam.current = true
-	Countdown.text = ""
+	if cam.current != null:
+		print("Tagged")
+		print(cam)
+
+		var Countdown = get_node("../../UI/RichTextLabel")
+		for i in range(3):
+			Countdown.text = str(i + 1)
+			await get_tree().create_timer(1.0).timeout
+			print(i)
+		cam.current = true
+		Countdown.text = ""
--- Assignments or challanges/Capstone/AYOOYM/Scripts/RayCast3D.gd
+++ Assignments or challanges/Capstone/AYOOYM/Scripts/RayCast3D.gd
@@ -3,14 +3,15 @@ extends RayCast3D
 @onready var cam = get_node("../CamNeck/Camera3D")
 
 func _physics_process(delta):
-	if is_colliding() && Input.is_action_pressed("CamSwitch"):
+	if is_colliding() && Input.is_action_pressed("CamSwitch") && get_collider().name == "Camera":
 		get_collider()._switch()
-		
+	else:
+		target_position.y = 0
 
 func _input(event):
 	if event.is_action_pressed("CamSwitch"):
-		target_position.z = -50
+		target_position.y = -50
 		print("Extend")
 	elif event.is_action_released("CamSwitch"):
-		target_position.z = -0
+		target_position.y = 0
 		print("Shortend")
