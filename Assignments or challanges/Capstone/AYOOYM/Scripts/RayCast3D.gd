c2238ee4b8f1c27a4169a02214cc481172f945b2
MarcusCheecham <6001808@spsd.sk.ca>
2025-05-15 12:18:33 -0600

    Trying to print and look for walls still

--- Assignments or challanges/Capstone/AYOOYM/Scripts/RayCast3D.gd
+++ Assignments or challanges/Capstone/AYOOYM/Scripts/RayCast3D.gd
@@ -1,17 +1,25 @@
 extends RayCast3D
 
-@onready var cam = get_node("../CamNeck/Camera3D")
+var collition = false
 
 func _physics_process(delta):
-	if is_colliding() && Input.is_action_pressed("CamSwitch") && get_collider().name == "Camera":
+	if is_colliding() && Input.is_action_pressed("CamSwitch") && get_collider().name == "Camera" && get_node("..").current == true:
+		collition = true
+		target_position.z = 0
+		print(get_collider())
 		get_collider()._switch()
-	else:
-		target_position.y = 0
+		await get_tree().create_timer(3.0).timeout
+		collition = false
+	elif get_node("..").current == true:
+		target_position.z = 0
 
 func _input(event):
-	if event.is_action_pressed("CamSwitch"):
-		target_position.y = -50
-		print("Extend")
-	elif event.is_action_released("CamSwitch"):
-		target_position.y = 0
-		print("Shortend")
+	if event.is_action_pressed("CamSwitch") && event.is_action_released("CamSwitch") && collition == true && get_node("..").current == true:
+		pass
+	elif collition == false && get_node("..").current == true:
+		if event.is_action_pressed("CamSwitch"):
+			target_position.z = -50
+			print("Extend")
+		elif event.is_action_released("CamSwitch"):
+			target_position.z = 0
+			print("Shortend")
