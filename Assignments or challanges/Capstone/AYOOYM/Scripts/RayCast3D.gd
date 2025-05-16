2631ceb2f4f53431546e7a7f653c16336de79126
MarcusCheecham <6001808@spsd.sk.ca>
2025-05-16 12:18:05 -0600

    Walls finally work at stopping rays
    The collision box for the area I was testing was not aligned properly with the mesh
    Now working on not allowing the player to tag two cameras within the three second wait time

--- Assignments or challanges/Capstone/AYOOYM/Scripts/RayCast3D.gd
+++ Assignments or challanges/Capstone/AYOOYM/Scripts/RayCast3D.gd
@@ -2,24 +2,35 @@ extends RayCast3D
 
 var collition = false
 
-func _physics_process(delta):
+	#if is_colliding() && Input.is_action_pressed("CamSwitch") && get_collider().name == "Camera" && get_node("..").current == true:
+		#collition = true
+		#target_position.z = 0
+		#print(get_collider())
+		#get_collider()._switch()
+		#await get_tree().create_timer(3.0).timeout
+		#collition = false
+	#elif get_node("..").current == true:
+		#target_position.z = 0
+		#
+	#if is_colliding() && get_collider().name != "Camera" && get_node("..").current == true:
+		#print(get_collider())
+		
+func _process(delta):
+	if is_colliding() && get_node("..").current == true && get_collider().name != "Camera":
+		collition = false
+		
 	if is_colliding() && Input.is_action_pressed("CamSwitch") && get_collider().name == "Camera" && get_node("..").current == true:
 		collition = true
-		target_position.z = 0
 		print(get_collider())
 		get_collider()._switch()
 		await get_tree().create_timer(3.0).timeout
 		collition = false
-	elif get_node("..").current == true:
-		target_position.z = 0
-
+		
 func _input(event):
-	if event.is_action_pressed("CamSwitch") && event.is_action_released("CamSwitch") && collition == true && get_node("..").current == true:
+	if collition == true && get_node("..").current == true:
 		pass
 	elif collition == false && get_node("..").current == true:
 		if event.is_action_pressed("CamSwitch"):
-			target_position.z = -50
-			print("Extend")
-		elif event.is_action_released("CamSwitch"):
-			target_position.z = 0
-			print("Shortend")
+			force_raycast_update()
+			print("Fire!")
+			collition = true
